a4e8a0011cdba82630a97d07c19ae5303c278ae6
Francesco Prospato <fprospato@gmail.com>
2019-06-26 15:42:20 -0700

    API to handle secret.xml

--- app/src/main/java/com/example/flicks/MovieListActivity.java
+++ app/src/main/java/com/example/flicks/MovieListActivity.java
@@ -21,7 +21,6 @@ public class MovieListActivity extends AppCompatActivity {
     //constants
     public final static String API_BASE_URL = "https://api.themoviedb.org/3"; //base url for api
     public final static String API_KEY_PARAM = "api_key"; //parameter name for API key
-    public final static String API_KEY = "a07e22bc18f5cb106bfe4cc1f83ad8ed"; //API key -- TODO move to secure location
     public final static String TAG = "MovieListActivity"; //tag for logging calls
 
 
@@ -51,7 +50,7 @@ public class MovieListActivity extends AppCompatActivity {
 
         //set request parameters (they're appended to the url)
         RequestParams params  = new RequestParams();
-        params.put(API_KEY_PARAM, API_KEY); //API key is always required
+        params.put(API_KEY_PARAM, getString(R.string.api_key)); //API key is always required
 
         //execute GET request (expecting JSON reponse)
         client.get(url, params, new JsonHttpResponseHandler() {
